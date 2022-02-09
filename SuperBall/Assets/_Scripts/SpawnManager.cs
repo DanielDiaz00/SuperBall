df4b2359a9db156f8edbfc073258172956c0d664
DanielDevGame <96780269+DanielDevGame@users.noreply.github.com>
2022-02-09 15:50:11 -0300

    Initial commit

---
+++ SuperBall/Assets/_Scripts/SpawnManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    private float spawnRange = 9;
+
+    public int enemyCount;
+    public int enemyWave = 1;
+
+    public GameObject powerUpPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(enemyWave);
+    }
+
+    void Update()
+    {
+        enemyCount = GameObject.FindObjectsOfType<Enemy>().Length;
+        if (enemyCount == 0)
+        {
+            //Se añade enemigos si no se encuentra alguno
+            enemyWave++;
+            SpawnEnemyWave(enemyWave);
+            
+            int numberOfPowerUps = Random.Range(0, 3);
+            for (int i = 0; i < numberOfPowerUps; i++)
+            {
+                Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+            }
+        }
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        //Ubicacion donde se van a regenerar
+        float spawnPosX = Random.Range(-spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+
+   
+    private void SpawnEnemyWave(int numberOfEnemies)
+    {
+        //Genera numero de enemigos en pantalla
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+    }
+    
+    
+    
+}
